607967908de2f8af2740addf91b8afac1fe35174
Sauer.Sebastian <Sauer.Sebastian@DESKTOP-UI62U22.2631.skool.net>
2019-11-08 09:45:35 +0100

    New Stuff

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,17 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("IZAV");
+		for (int number= 3; number <= 1000000000; number++) {
+			boolean isPrime = true;
+			for (int i = 2; i <= Math.sqrt(number) && isPrime; i++) {
+				if (number % i == 0) {
+					isPrime = false;
+				}
+				//System.out.println(i);
+			}
+			if (isPrime) {
+				System.out.println(number + " ");
+			}
+		}
 	}
-
 }
