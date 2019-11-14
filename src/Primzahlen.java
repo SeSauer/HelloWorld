66e8d7bb77f8b4b82cc441e4dc7e9ae0de8b2f53
Sauer.Sebastian <Sauer.Sebastian@DESKTOP-UI62U22.2631.skool.net>
2019-11-14 12:14:20 +0100

    ...

--- src/Primzahlen.java
+++ src/Primzahlen.java
@@ -4,22 +4,22 @@ public class Primzahlen {
 	static int length = 100000;
 
 	public static void main(String[] args) {
-		long timer=System.nanoTime();
-		
-		boolean[] primzahlen = new boolean[length+1];
+		long timer = System.nanoTime();
+
+		boolean[] primzahlen = new boolean[length + 1];
 		Arrays.fill(primzahlen, Boolean.TRUE);
 		for (int i = 2; i <= length; i++) {
 
 			if (primzahlen[i]) {
 				for (int j = i; j <= length; j = j + i) {
-					primzahlen[j]=false;
+					primzahlen[j] = false;
 				}
-			System.out.println(i);
+				System.out.println(i);
 			}
 
 		}
-		timer=System.nanoTime()-timer;
-		System.out.println("Laufzeit: "+timer/1000000+" ms");
+		timer = System.nanoTime() - timer;
+		System.out.println("Laufzeit: " + timer / 1000000 + " ms");
 	}
 
 }
